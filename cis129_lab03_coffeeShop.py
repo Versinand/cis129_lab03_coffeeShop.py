b657e7a34eca4bbbccd2dc00a5e1e07ab57c4c5b
Versinand <jperez124@mail.pima.edu>
2024-09-13 16:51:08 -0700

    Update cis129_lab03_coffeeShop.py
    Fixed minor print errors and some text

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -7,7 +7,7 @@ tax_rate = 0.06
 
 # Header for the shop
 print("***************************************")
-print("My Coffee and Muffin Shop")
+print("My Cafe")
 
 # User input
 num_coffees = int(input("Number of coffees bought? "))
@@ -19,7 +19,7 @@ subtotal_coffee = num_coffees * coffee_price
 subtotal_muffin = num_muffins * muffin_price
 subtotal_tea = num_teas * tea_price
 subtotal_cake = num_cakes * cake_price
-subtotal = subtotal_coffee + subtotal_muffin + subtotal_tea + subtotal_cakes
+subtotal = subtotal_coffee + subtotal_muffin + subtotal_tea + subtotal_cake
 
 # Calculate tax
 tax = subtotal * tax_rate
@@ -29,7 +29,7 @@ total = subtotal + tax
 
 # Receipt header
 print("***************************************")
-print("My Coffee and Muffin Shop Receipt")
+print("My Cafe")
 
 # Detailed receipt
 if num_coffees > 0:
@@ -37,14 +37,14 @@ if num_coffees > 0:
 if num_muffins > 0:
     print(f"{num_muffins} Muffin{'s' if num_muffins > 1 else ''} at ${muffin_price} each: ${subtotal_muffin:.2f}")
 if num_teas > 0:
-    print(f"{num_teas} Coffee{'s' if num_teas > 1 else ''} at ${tea_price} each: ${subtotal_tea:.2f}")
+    print(f"{num_teas} Tea{'s' if num_teas > 1 else ''} at ${tea_price} each: ${subtotal_tea:.2f}")
 if num_cakes > 0:
-    print(f"{num_cakes} Muffin{'s' if num_cakes > 1 else ''} at ${cake_price} each: ${subtotal_cake:.2f}")
+    print(f"{num_cakes} Cake{'s' if num_cakes > 1 else ''} at ${cake_price} each: ${subtotal_cake:.2f}")
 
 
 print(f"6% tax: ${tax:.2f}")
 print("---------")
 print(f"Total: ${total:.2f}")
 print("Thank you")
-Print("Please come again soon :D")
+Print("Please come again soon ;D")
 print("***************************************")
